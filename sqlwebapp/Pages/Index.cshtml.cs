45b752ea78074a3081877700ff5c5eff69048d2a
Christoph <c@schmalisch.de>
2022-11-14 11:57:21 +0100

    Projektdateien hinzufügen.

---
+++ sqlwebapp/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using sqlwebapp.models;
+using sqlwebapp.Services;
+
+namespace sqlwebapp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public List<Product> Products;
+        public void OnGet()
+        {
+            ProductService productService = new ProductService();
+            Products=productService.GetProducts();
+
+        }
+    }
+}
